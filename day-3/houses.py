9b840e2c6ac561ba7e2b39e94a4cf5fd0eac0aa8
sid-707 <95631365+sid-707@users.noreply.github.com>
2023-06-07 22:56:39 -0400

    Day 3 Part 2

--- day-3/houses.py
+++ day-3/houses.py
@@ -1,20 +1,28 @@
+def update_position(current_pos, move):
+    match move:
+        case '>':
+            current_pos[0] += 1
+        case '<':
+            current_pos[0] -= 1
+        case '^':
+            current_pos[1] += 1
+        case 'v':
+            current_pos[1] -= 1
+    
+    return current_pos
+
 def count_number_of_houses(moves):
-    current_position = [0, 0]
+    position = {
+        0: [0,0], # santa
+        1: [0,0] # robo
+    }
 
     number_of_houses = 1
 
     houses_visited = { '0,0' }
-    for move in moves:
-        match move:
-            case '>':
-                current_position[0] += 1
-            case '<':
-                current_position[0] -= 1
-            case '^':
-                current_position[1] += 1
-            case 'v':
-                current_position[1] -= 1
-        current_pos_str = ','.join(str(x) for x in current_position)
+    for (index, move) in enumerate(moves):
+        new_pos = update_position(position[index % 2], move)
+        current_pos_str = ','.join(str(x) for x in new_pos)
         if current_pos_str not in houses_visited:
             number_of_houses += 1
             houses_visited.add(current_pos_str)
--- day-3/test_houses.py
+++ day-3/test_houses.py
@@ -4,11 +4,11 @@ from houses import count_number_of_houses
 
 class TestHouses(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(count_number_of_houses('>'), 2)
+        self.assertEqual(count_number_of_houses('^v'), 3)
     def test_2(self):
-        self.assertEqual(count_number_of_houses('^>v<'), 4)
+        self.assertEqual(count_number_of_houses('^>v<'), 3)
     def test_3(self):
-        self.assertEqual(count_number_of_houses('^v^v^v^v^v'), 2)
+        self.assertEqual(count_number_of_houses('^v^v^v^v^v'), 11)
 
 
 if __name__ == "__main__":
